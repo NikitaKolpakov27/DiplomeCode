82e0af9c6ee668afad36f6de38d28bed582029d8
NikitaKolpakov27 <nik.kolpakov@inbox.ru>
2025-04-23 22:34:24 +0300

    Daemon starts work! Needed abs path in file

--- service/daemon_test.py
+++ service/daemon_test.py
@@ -2,7 +2,7 @@ import datetime
 import time
 
 while True:
-    with open("test_file.txt", "a+") as f:
+    with open("D:\\На новый ноут\\PyCharm proj\\diplome_code\\service\\test_file.txt", "a+") as f:
         f.write(str(datetime.datetime.now()) + " Pop\n")
     f.close()
     time.sleep(2)
