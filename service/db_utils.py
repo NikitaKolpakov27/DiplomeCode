0583ff4a204e8171c7c8b6fcebeb281fd21fb8a0
NikitaKolpakov27 <nik.kolpakov@inbox.ru>
2025-06-01 22:29:58 +0300

    JSON DB instead of .txt (common files only)

--- service/db_utils.py
+++ service/db_utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 from service.conf_utils import is_file_or_text_confidential, get_conf_files
 from service.file_utils import hash_file
@@ -5,9 +6,17 @@ from service.file_utils import hash_file
 
 def update_db():
     cur_path = os.path.dirname(__file__)
-    db_path = os.path.relpath("..\\view\\fileDatabase.txt", cur_path)
+    db_path_json = os.path.relpath("..\\view\\fileDB.json", cur_path)
 
-    open(db_path, 'w', encoding="utf-8").close()
+    # Сначала очищаем БД
+    open(db_path_json, 'w', encoding="utf-8").close()
+
+    # Потом записываем "основу" (чтобы не было ошибок при получении данных)
+    with open(db_path_json, 'w', encoding="utf-8") as file:
+        db_str = "{\"files\":[]}"
+        file.write(db_str)
+    file.close()
+
     _get_file_hashes()
 
 
@@ -17,13 +26,18 @@ def _write_file_to_db(elem):
     """
 
     cur_path = os.path.dirname(__file__)
-    db_path = os.path.relpath("..\\view\\fileDatabase.txt", cur_path)
+    db_path_json = os.path.relpath("..\\view\\fileDB.json", cur_path)
 
     text_conf = is_file_or_text_confidential(False, elem)
     hash_elem = hash_file(elem)
 
-    with open(db_path, "a", encoding="utf-8") as file:
-        file.write(elem + "-----" + hash_elem + "-----" + str(text_conf) + "\n")
+    # Работа с БД на JSON
+    with open(db_path_json, "r+", encoding="utf-8") as file:
+        fileDB = json.load(file)
+
+        fileDB["files"].append({"file_path": elem, "file_hash": hash_elem, "status": text_conf})
+        file.seek(0)
+        json.dump(fileDB, file, indent=4)
 
     file.close()
 
@@ -57,4 +71,3 @@ def update_conf_db():
 
     open(db_path, 'w', encoding="utf-8").close()
     make_conf_file_db()
-
